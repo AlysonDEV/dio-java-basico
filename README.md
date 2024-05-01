84b3e04503ba3be3febc2cc8f025f9346e8cfed5
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-05-01 08:32:05 -0300

    Terminando os estudos de Tipos e Variáveis

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dio-java-basico
 Repositório para armazenar todo o conteúdo de java básico.
+
+## Tipos e variaveis
+
+
+| Tipo | Memória | Valor Mínimo | Valor Máximo |
+| ---- | ---- | - | - |
+| byte | 1 byte | -128 | 127 |
+| short | 2 byte | -32.768 | 32.767 |
+| int | 4 bytes | -2.147.483.648 | 2.147.483.647 | 
+| long | 8 bytes | -9.223.372.036.854.775.808 | 9.223.372.036.854.775.807
+
+
+| Tipo | Memória | Valor Mínimo | Valor Máximo | Precisão |
+| --- | --- | --- | --- | --- |
+| float | 4 bytes | -3,4028E + 38 | 3,4028E + 38 | 6 - 7 dígitos |
+| double | 8 bytes | -1,7976E + 308 | 1,7976E + 308 | 15 dígitos |
+
+
+## Fonte de pesquisas
+
+- [ ] [Documentação para estudo: glysns](https://glysns.gitbook.io/java-basico/sintaxe/variaveis)
---
+++ src/edu/alyson/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,47 @@
+package edu.alyson.tiposvariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Apreendendo sobre tipos e variáveis");
+        double salarioMinimo = 2.5;
+        System.out.println("O salário mínimo é: " + salarioMinimo);
+
+        byte idade = 123;
+        imprimir("Idade: ", String.valueOf(idade));
+        short ano = 2021;
+        imprimir("Ano: ", String.valueOf(ano));
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        imprimir("CEP: ", String.valueOf(cep));
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        imprimir("CPF: ", String.valueOf(cpf));
+        float pi = 3.14F;
+        imprimir("PI: ", String.valueOf(pi));
+        double salario = 1275.33;
+        imprimir("Salário: ", String.valueOf(salario));
+
+    }
+
+    public static void imprimir(String... args) {
+        final String MENSAGEM = "Método para imprimir informações: ";
+        final byte DIVISOR = 40;
+
+        byte totalImpressoes = 0;
+
+        for (String argsString : args) {
+            Separador(DIVISOR);
+            if (totalImpressoes == 0) {
+                System.out.println(MENSAGEM);
+            }
+            System.out.println(argsString);
+            totalImpressoes++;
+        }
+    }
+
+    public static void Separador(Byte total) {
+        for (byte i = 0; i < total; i++) {
+            System.out.print("-");
+        }
+        System.out.println(""); // Apenas para quebrar a linha
+
+    }
+}
