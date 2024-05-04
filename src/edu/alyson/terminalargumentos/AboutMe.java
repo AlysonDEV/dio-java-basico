d52bc431661592c4d457f8bd8f8c75b49c0076a4
Alyson Ronnan Martins <devalyson@gmail.com>
2024-05-04 20:55:18 -0300

    Finalizando terminais e argumentos

--- src/edu/alyson/terminalargumentos/AboutMe.java
+++ src/edu/alyson/terminalargumentos/AboutMe.java
@@ -2,6 +2,14 @@ package edu.alyson.terminalargumentos;
 
 public class AboutMe {
     public static void main(String[] args) {
-        System.out.println("Tela sobre a a aplicação");
+        //os argumentos começam com indice 0
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
     }
 }
